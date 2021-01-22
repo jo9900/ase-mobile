b791e8b1f8ede038be2c8d90d96ea8527f6aa978
郑媛 <zhengyuan@vbhledger.com>
2021-01-22 15:58:14 +0800

    修改密码后不跳转

--- src/views/Center/password.vue
+++ src/views/Center/password.vue
@@ -157,7 +157,7 @@ export default {
                 message: this.$t("message.91"),
                 type: "success",
               });
-              this.$router.push("center");
+              // this.$router.push("center");
             } else if (res.code == 101503) {
               this.$message.error(this.$t("message.302"));
             } else if (res.code == 101504) {
