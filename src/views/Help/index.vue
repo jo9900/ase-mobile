dea96989b355992aac95927e7fe3d5d0f18bf857
郑媛 <zhengyuan@vbhledger.com>
2021-01-22 15:00:07 +0800

    帮助英文图片

--- src/views/Help/index.vue
+++ src/views/Help/index.vue
@@ -8,13 +8,13 @@
       <a class="link" href="https://m.arthurex.com/signIn">https://m.arthurex.com/signIn</a>
       {{ text[4] }}
     </p>
-    <img src="@/assets/images/help1.jpg" alt="help1">
+    <img :src="imgs[1]" alt="help1">
     <p>
       {{ text[5] }}
       <a class="link" href="https://m.arthurex.com">https://m.arthurex.com</a>
     </p>
     <p>{{ text[6] }}</p>
-    <img src="@/assets/images/help2.jpg" alt="help2">
+    <img :src="imgs[2]" alt="help2">
     <p>
       {{ text[7] }}
       <a class="link" href="https://m.arthurex.com/signIn">https://m.arthurex.com/signIn</a>
@@ -22,35 +22,35 @@
     <p class="h3">{{ text[8] }}</p>
     <p>
       {{ text[9] }}
-      <img class="icon" src="@/assets/images/help3.png" alt="icon">
+      <img class="icon" src="@/assets/images/help/help3.png" alt="icon">
       {{ text[10] }}
     </p>
-    <img  src="@/assets/images/help4.jpg" alt="help4">
+    <img  :src="imgs[4]" alt="help4">
     <p class="h3">{{ text[11] }}</p>
     <p>{{ text[12] }}</p>
-    <img  src="@/assets/images/help5.jpg" alt="help5">
+    <img  :src="imgs[5]" alt="help5">
     <p>
       {{ text[13] }}
       <a class="link" href="https://m.arthurex.com/presell">https://m.arthurex.com/presell</a>
     </p>
     <p>
       {{ text[14] }}
-      <img class="check" src="@/assets/images/help6.jpg" alt="help6">
+      <img class="check" :src="imgs[6]" alt="help6">
       {{ text[15] }}
     </p>
 
-    <img src="@/assets/images/help7.jpg" alt="help7">
+    <img :src="imgs[7]" alt="help7">
     <p>{{ text[16] }}</p>
     <p class="h3">{{ text[17] }}</p>
     <p>{{ text[18] }}</p>
     <p>{{ text[19] }}</p>
-    <img src="@/assets/images/help8.jpg" alt="help8">
+    <img :src="imgs[8]" alt="help8">
     <p>{{ text[20] }}</p>
     <p>{{ text[21] }}</p>
-    <img src="@/assets/images/help9.jpg" alt="help9">
+    <img :src="imgs[9]" alt="help9">
     <p class="h3">{{ text[22] }}</p>
     <p>{{ text[23] }}</p>
-    <img src="@/assets/images/help10.png" alt="help10">
+    <img :src="imgs[10]" alt="help10">
     <p class="h3">{{ text[24] }}</p>
     <p>{{ text[25] }}</p>
     <p>{{ text[26] }}</p>
@@ -66,11 +66,26 @@
     name: "",
     data() {
       return {
+        imgs:{
+
+        },
         text: {}
       }
     },
     created() {
+      let lang = this.$lang
       this.text = text[this.$lang]
+      this.imgs = {
+        1: require(`../../assets/images/help/help1-${lang}.png`),
+        2: require(`../../assets/images/help/help2-${lang}.png`),
+        4: require(`../../assets/images/help/help4-${lang}.png`),
+        5: require(`../../assets/images/help/help5-${lang}.png`),
+        6: require(`../../assets/images/help/help6-${lang}.png`),
+        7: require(`../../assets/images/help/help7-${lang}.png`),
+        8: require(`../../assets/images/help/help8-${lang}.png`),
+        9: require(`../../assets/images/help/help9-${lang}.png`),
+        10:require(`../../assets/images/help/help10-${lang}.png`),
+      }
     }
 
   };
