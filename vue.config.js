52a0bb364e3a1be2629739bb06f357b2a6ef6957
郑媛 <zhengyuan@vbhledger.com>
2021-02-01 16:24:24 +0800

    优化 打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
-const productionGzipExtensions = ["js", "css"];
-const isProduction = process.env.NODE_ENV === "production";
+const productionGzipExtensions = /\.(js|css|json|txt|html)(\?.*)?$/i;
+const TerserWebpackJsPlugin = require("terser-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
     publicPath: "./",
     productionSourceMap: false,
@@ -33,28 +35,58 @@ module.exports = {
 
     pluginOptions: {},
     configureWebpack: (config) => {
-        if (isProduction) {
+        if (isProd) {
             config.plugins.push(
                 new CompressionWebpackPlugin({
                     algorithm: "gzip",
-                    test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+                    test: productionGzipExtensions,
                     threshold: 10240,
                     minRatio: 0.8,
                 })
             );
-        }
-        if (process.env.NODE_ENV === "production") {
-            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+            config.plugins.push(
+                new TerserWebpackJsPlugin({
+                    terserOptions: {
+                        compress: {
+                            warnings: false,
+                            drop_console: true,
+                            drop_debugger: false,
+                            pure_funcs: ["console.log"] //移除console
+                        }
+                    },
+                    sourceMap: false,
+                    parallel: true
+                })
+            )
         }
     },
 
     chainWebpack: (config) => {
+
+        config.output.filename('./assets/js/[name].[chunkhash:8].js');
+        config.output.chunkFilename('./assets/js/[name].[chunkhash:8].js');
+
         config.resolve.symlinks(true);
-    },
-    configureWebpack: {
-        externals: {
+        config.set('externals', {
             vue: "Vue",
             "element-ui": "ELEMENT",
-        },
-    },
+        })
+        config.optimization.splitChunks({
+            cacheGroups: {
+                common: {
+                    name: 'common',
+                    chunks: 'all',
+                    minSize: 1,
+                    minChunks: 2,
+                    priority: 1
+                },
+                vendor: {
+                    name: 'chunk-libs',
+                    chunks: 'all',
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: 10
+                }
+            }
+        });
+    }
 };
