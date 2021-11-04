23a59c0c6619e0f24fd2092c2c352d31a1f95803
lekai.lin <linlekai@vbhledger.com>
2021-11-04 09:48:35 +0800

    调整接口与跳转

--- src/request/http.js
+++ src/request/http.js
@@ -6,9 +6,9 @@ axios.defaults.withCredentials = true;
 
 let baseUrl = "";
 if (process.env.NODE_ENV == "development") {
-    baseUrl = "";
-} else if (process.env.NODE_ENV == "production") {
-    baseUrl = process.env.VUE_APP_BASE_API;
+    baseUrl = window.location.origin + '/'
+} else {
+    baseUrl = window.location.origin + '/'
 }
 
 axios.defaults.baseURL = baseUrl;
